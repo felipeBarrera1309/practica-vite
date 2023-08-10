5c70ad6c4900dbeb2bacd98afabd22865757df76
Felipe Barrera <felipebarrera.gbp@gmail.com>
2023-08-10 17:15:12 -0500

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // vite.config.js
 export default defineConfig(({ command, mode }) => {
-    const port = 8080;
+    const port = 5050;
+
+    const seeEnv = loadEnv(mode, process.cwd())
+    console.log('Estas son las variables de entorno que me trae: ', seeEnv);
 
     console.log('Esto se ejecuta en el momento de poner en desarrollo nuestro proyecto y trae estos dos valores: ', command, mode)
 
     return {
+        // configuracion de vite
+        // vite config
         server: {
             port
         }
